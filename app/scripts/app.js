0020b51be0b69027e7efa51a5e703ff5f6928682
joao.buch <joao.buch@inlog.com.br>
2017-04-26 16:56:34 -0300

    Ranking com proximo comprador

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,3 @@
 (function(){
-	angular.module('coffee', ['ui.utils.masks', 'idf.br-filters']);
+	angular.module('coffee', ['ui.utils.masks']);
 })();
--- app/scripts/coffee.controller.js
+++ app/scripts/coffee.controller.js
@@ -3,28 +3,37 @@
         .module('coffee')
         .controller('coffeeController', function ($scope, myCoffees) {
             $('.datepicker').pickadate({
+                format: 'dd/mm/yyyy',
                 selectMonths: true, // Creates a dropdown to control month
                 selectYears: 15 // Creates a dropdown of 15 years to control year
             });
 
+            $scope.next_buyer = {};
+              
+
             function resetObjects() {
                 $scope.users = [];
+                $scope.usersList = [];
                 $scope.userInsert = { name: undefined, totalValue: 0, totalQtd: 0, itens: [{ date: new Date(), value: 0, weight: 0, qtd: 0 }] };
-                $scope.userUpdate = { name: undefined, totalValue: 0, totalQtd: 0, itens: [{ date: new Date(), value: 0, weight: 0, qtd: 0 }] };
+                $scope.userUpdate = { name: undefined, totalValue: null, totalQtd: 0, itens: [{ date: new Date(), value: null, weight: 50, qtd: 1 }] };
             }
 
             $scope.updateUser = function (userUpdate) {
-                debugger;
-                var user = $scope.users.find(function(item){ return item.id === userUpdate.id; });
-
-                if(user){
+                
+                var user = $scope.users.find(function (item) { return item.id === userUpdate.id; });
+                user.totalQtd = parseInt(user.totalQtd) + parseInt(userUpdate.itens[0].qtd);
+                user.last_buy = userUpdate.itens[0].date;
+                if (user) {
+                  
+                    if(!user.itens) user.itens = [];
                     user.itens.push(userUpdate.itens[0]);
-                    myCoffees.update(user).then(function(){
+                    myCoffees.update(user).then(function () {
                         resetObjects();
                         loadUsers();
                         $(novo).removeClass('active');
                         $(historico).addClass('active');
-                        $scope.$apply();
+                        location.reload(true);
+                       
                     });
                 }
             }
@@ -33,7 +42,22 @@
 
                 myCoffees.listar().then(function (users) {
                     $scope.users = users;
-                })
+
+                    $scope.users.forEach(function (user) {
+                        if(!$scope.next_buyer.last_buy || user.last_buy < $scope.next_buyer.last_buy){
+                            $scope.next_buyer = user;
+                        }
+                        if(user.itens) user.itens.forEach(function (item) {
+                            $scope.usersList.push({
+                                name: user.name,
+                                date: item.date,
+                                value: item.value,
+                                qtd: item.qtd,
+                                weight: item.weight
+                            });
+                        });
+                    });
+                });
             }
 
             loadUsers();
